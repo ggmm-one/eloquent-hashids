fa5b47e2ed4409f9cbb9c525d2de800f4d2569b6
ggmm-one <ggmm@gmm.one>
2020-12-25 21:00:36 +1100

    Add builder macros

--- src/HasHashid.php
+++ src/HasHashid.php
@@ -4,7 +4,7 @@ namespace Ggmm\Model;
 
 use Hashids\Hashids;
 use Hashids\HashidsInterface;
-use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Eloquent\Builder;
 
 trait HasHashid
 {
@@ -29,57 +29,23 @@ trait HasHashid
         }
     }
 
-    /**
-     * Find a model by its primary key.
-     *
-     * @param  mixed  $id
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
-     */
-    public function findByHashid($id, $columns = ['*'])
+    public static function bootHasHashid()
     {
-        return $this->find($this-decodeHashidForQueryBuilder($id), $columns);
+        Builder::macro('findByHashid', function ($id, $columns = ['*']) {
+            return $this->find($this->getModel()->decodeHashidForQueryBuilder($id), $columns);
+        });
+        Builder::macro('findManyByHashid', function ($ids, $columns = ['*']) {
+            return $this->find($this->getModel()->decodeHashidForQueryBuilder($ids), $columns);
+        });
+        Builder::macro('findOrFailByHashid', function ($id, $columns = ['*']) {
+            return $this->findOrFail($this->getModel()->decodeHashidForQueryBuilder($id), $columns);
+        });
+        Builder::macro('findOrNewByHashid', function ($id, $columns = ['*']) {
+            return $this->findOrNew($this->getModel()->decodeHashidForQueryBuilder($id), $columns);
+        });
     }
 
-    /**
-     * Find multiple models by their primary keys.
-     *
-     * @param  \Illuminate\Contracts\Support\Arrayable|array  $ids
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    public function findManyByHashid($ids, $columns = ['*'])
-    {
-        return $this->findMany($this-decodeHashidForQueryBuilder($ids), $columns);
-    }
-
-    /**
-     * Find a model by its primary key or throw an exception.
-     *
-     * @param  mixed  $id
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static|static[]
-     *
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     */
-    public function findOrFailByHashid($id, $columns = ['*'])
-    {
-        return $this->findOrFail($this-decodeHashidForQueryBuilder($id), $columns);
-    }
-
-    /**
-     * Find a model by its primary key or return fresh model instance.
-     *
-     * @param  mixed  $id
-     * @param  array  $columns
-     * @return \Illuminate\Database\Eloquent\Model|static
-     */
-    public function findOrNewByHashid($id, $columns = ['*'])
-    {
-        return $this->findOrNew($this-decodeHashidForQueryBuilder($id), $columns);
-    }
-
-    private function decodeHashidForQueryBuilder($ids)
+    public function decodeHashidForQueryBuilder($ids)
     {
         if (is_null($ids)) {
             return null;
@@ -87,14 +53,14 @@ trait HasHashid
         if ($ids instanceof Arrayable) {
             $ids = $ids->toArray();
         }
-        if (!is_array($ids)) {
-            $ids = [$ids];
+        if (is_array($ids)) {
+            $ids = array_map(function ($v) {
+                return $this->decodeHashid($v);
+            }, $ids);
+        } else {
+            $ids = $this->decodeHashid($ids);
         }
 
-        $model = new static();
-
-        return array_map(function ($v) use ($model) {
-            return $model->decodeHashid($v);
-        }, $ids);
+        return $ids;
     }
 }
